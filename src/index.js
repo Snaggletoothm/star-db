4ccf90b08cd9098f9e8333d7835b992dde7e9441
Mikriukov MA <snagglet00th@yandex.ru>
2022-04-02 20:47:04 +0500

    raise and catch errors into fetch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if(!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
+
   const body = await res.json();
   return body;
 };
 
 getResource('https://swapi.dev/api/people/1/')
-  .then(body => console.log(body));
+  .then(body => console.log(body))
+  .catch(err => console.log(`>>> error is apeared: `, err));
